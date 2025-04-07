720a6e2874479be7c893d33cbbd31c3a3663c404
Alejandra Coeto <a01285221@tec.mx>
2025-04-07 11:53:12 -0600

    Update reqs

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, Response
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
+from pydantic import BaseModel
+from Embeddings.embeddings import Embeddings
 
 app = FastAPI()
 app.add_middleware(
@@ -10,10 +12,21 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+emb = Embeddings()
+
+class TextRequest(BaseModel):
+    text: str
 
 @app.get("/")
 def general():
     return {"Message": "Hello World"}
+
+@app.post("/embedding")
+def embedding(req: TextRequest):
+    text = req.text
+    print(f"Received text: {text}")
+    embedding = emb.generate_embeddings([text])
+    return {"embedding": embedding[0].tolist()}
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
